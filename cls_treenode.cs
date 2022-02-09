6ed27a36e5c904a2dbab68b7d9cf828bc365f1e9
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-02-09 22:45:00 +0900

    Create cls_treenode.cs

---
+++ cls_treenode.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * SharpDevelopによって生成
+ * ユーザ: 2566haraya
+ * 日付: 2022/02/09
+ * 時刻: 14:24
+ * 
+ * このテンプレートを変更する場合「ツール→オプション→コーディング→標準ヘッダの編集」
+ */
+using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace IMG_Gen2
+{
+	/// <summary>
+	/// Description of cls_treenode.
+	/// </summary>
+	public class cls_treenode : TreeNode
+	{
+		private string nodePath;
+		private cls_treenode[] node = new cls_treenode[0];
+		
+		public cls_treenode(string path, string nodeName)
+		{
+			nodePath = nodeName + "\\";
+			this.Text = nodeName.Replace(path, "");
+			
+			if (Directory.Exists(nodeName))
+			{
+				System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(nodePath);
+				System.IO.FileInfo[] files = di.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
+				foreach (System.IO.FileInfo f in files)
+				{
+					Array.Resize(ref node, node.Length + 1);
+					node[node.Length - 1] = new cls_treenode(nodePath, f.FullName);
+				}
+				this.Nodes.AddRange(node);
+			}
+		}
+	}
+}
