0e880b5ad95c46617e965af4f2b0754d26e9fafe
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-02-12 22:40:20 +0900

    Update MainForm.cs

--- MainForm.cs
+++ MainForm.cs
@@ -1,3 +1,4 @@
+using Microsoft.Win32.SafeHandles;
 namespace IMG_Gen2;
 
 public partial class MainForm : Form
@@ -15,6 +16,7 @@ public partial class MainForm : Form
 
     private void InitializeUserComponent()
     {
+
         //sLabel1
         this.sLabel1 = new ToolStripStatusLabel();
         this.sLabel1.Text = "sLabel1";
@@ -25,9 +27,7 @@ public partial class MainForm : Form
         this.sLabel2.Text = "sLabel2";
         this.StatusStrip2.Items.Add(sLabel2);
 
-        //
         // PicBox1
-        //
         this.PosPicBox = new cls_posPicBox(PosPage, sLabel2!);
         this.PosPicBox.TabIndex = 22;
         this.PosPicBox.Text = "PictureBox0";
@@ -37,9 +37,7 @@ public partial class MainForm : Form
         this.PosPicBox.Dock = System.Windows.Forms.DockStyle.Fill;
         this.PosPage.Controls.Add(this.PosPicBox);
 
-        //
         // FileTreeView
-        //
         this.FileTreeView = new cls_treeview(PosPicBox!, PosPage);
         this.FileTreeView.ItemHeight = 22;
         this.FileTreeView.LineColor = System.Drawing.Color.Black;
@@ -50,87 +48,133 @@ public partial class MainForm : Form
         this.FileTreeView.TabIndex = 23;
         this.FilePage.Controls.Add(this.FileTreeView);
 
-        //
         // LabelLstView
-        //
-        LabelLstView.FullRowSelect = true;
-
         ColumnHeader[] column = new ColumnHeader[3];
         for (int i = 0; i < column.Count(); i++)
         {
             column[i] = new();
         }
-        // column[0].Width = -2;
-        // column[1].Width = -2;
-        // column[2].Width = -2;
-        column[0].Text = "ラベル";
-        column[1].Text = "線色";
-        column[2].Text = "線幅";
+        column[0].Text = "ラ　ベ　ル";
+        column[1].Text = "線　色";
+        column[2].Text = "線　幅";
         this.LabelLstView.Columns.AddRange(column);
-
-        ListViewItem item;
-
-        item = LabelLstView.Items.Add("NG");
-        item.SubItems.Add("Color [Red]");
-        item.SubItems.Add("1");
-        item.UseItemStyleForSubItems = false;
-        item.SubItems[1].BackColor = Color.Red;
+        LabelLstView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
     }
 
     private void Controls_EventHandler()
     {
-        //
+        // this
+        this.Load += new EventHandler(Form_Load);
+
+        LabelLstView.SelectedIndexChanged += new EventHandler(lblLstView_SelectedIndexChanged);
+
         // RootSelectBtn
-        //
         RootSelectBtn.Click += new EventHandler(RootSelectBtn_Click);
 
-        //
         // LabelColorBtn
-        //
         LabelColorBtn.Click += new EventHandler(LabelColorBtn_Click);
 
-        //
         // LabelEntBtn
-        //
         LabelEntBtn.Click += new EventHandler(LabelEntBtn_Click);
 
+        // LabelDelBtn
+        LabelDelBtn.Click += new EventHandler(LabelDelBtn_Click);
+
+        //LabelModBtn
+        LabelModBtn.Click += new EventHandler(LabelModBtn_Click);
+
+    }
+
+    private void Form_Load(object? sender, EventArgs e)
+    {
+        StreamReader sr = new StreamReader("./label.ini");
+        ListViewItem item;
+
+        while (!sr.EndOfStream)
+        {
+            string? line = sr.ReadLine();
+            string[] values = line!.Split(',');
+
+            item = new();
+            item = LabelLstView.Items.Add(values[0]);
+            item.SubItems.Add(values[1]);
+            item.SubItems.Add(values[2]);
+            item.UseItemStyleForSubItems = false;
+            item.SubItems[1].BackColor = String2Color(values[1]);
+        }
+        sr.Close();
+    }
+
+    private void LabelModBtn_Click(Object? sender, EventArgs? e)
+    {
+        if (LabelLstView.SelectedItems.Count == 0) { return; }
+
+        LabelLstView.SelectedItems[0].SubItems[0].Text = LabelNameTxtBox.Text;
+        LabelLstView.SelectedItems[0].SubItems[1].Text = LabelColorTxtBox.Text;
+        LabelLstView.SelectedItems[0].SubItems[2].Text = LabelWidthTxtBox.Text;
+        LabelLstView.SelectedItems[0].UseItemStyleForSubItems = false;
+        LabelLstView.SelectedItems[0].SubItems[1].BackColor = String2Color(LabelColorTxtBox.Text);
+
+        LabelLstView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        SavaLabelIni();
+    }
+
+    private void lblLstView_SelectedIndexChanged(object? sender, EventArgs e)
+    {
+        if (LabelLstView.SelectedItems.Count == 0) { return; }
+
+        LabelNameTxtBox.Text = LabelLstView.SelectedItems[0].SubItems[0].Text;
+        LabelColorTxtBox.Text = LabelLstView.SelectedItems[0].SubItems[1].Text;
+        LabelWidthTxtBox.Text = LabelLstView.SelectedItems[0].SubItems[2].Text;
+    }
+
+    private void LabelDelBtn_Click(Object? sender, EventArgs? e)
+    {
+        if (LabelLstView.SelectedItems.Count == 0) { return; }
+
+        LabelLstView.Items[LabelLstView.SelectedItems[0].Index].Remove();
+
+        LabelLstView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        SavaLabelIni();
     }
 
     private void LabelEntBtn_Click(Object? sender, EventArgs? e)
     {
+        if (LabelNameTxtBox.Text == "") { return; }
+
         ListViewItem item;
         item = LabelLstView.Items.Add(LabelNameTxtBox.Text);
         item.SubItems.Add(LabelColorTxtBox.Text);
         item.SubItems.Add(LabelWidthTxtBox.Text);
         item.UseItemStyleForSubItems = false;
-        item.SubItems[1].BackColor = Color.Blue;
+        item.SubItems[1].BackColor = String2Color(LabelColorTxtBox.Text);
 
-        LabelLstView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+        LabelLstView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        SavaLabelIni();
+    }
+
+    private void SavaLabelIni()
+    {
+        StreamWriter sw = new StreamWriter("./label.ini");
+
+        for (int i = 0; i < LabelLstView.Items.Count; i++)
+        {
+            sw.WriteLine(LabelLstView.Items[i].SubItems[0].Text + "," +
+                         LabelLstView.Items[i].SubItems[1].Text + "," +
+                         LabelLstView.Items[i].SubItems[2].Text);
+        }
+        sw.Flush();
+        sw.Close();
     }
     private void LabelColorBtn_Click(Object? sender, EventArgs? e)
     {
-        //ColorDialogクラスのインスタンスを作成
         ColorDialog cd = new ColorDialog();
-
-        //はじめに選択されている色を設定
-        // cd.Color = TextBox1.BackColor;
-        //色の作成部分を表示可能にする
-        //デフォルトがTrueのため必要はない
         cd.AllowFullOpen = true;
-        //純色だけに制限しない
-        //デフォルトがFalseのため必要はない
         cd.SolidColorOnly = false;
-        //[作成した色]に指定した色（RGB値）を表示する
-        // cd.CustomColors = new int[] {
-        //                             0x33, 0x66, 0x99, 0xCC, 0x3300, 0x3333,
-        //                             0x3366, 0x3399, 0x33CC, 0x6600, 0x6633,
-        //                             0x6666, 0x6699, 0x66CC, 0x9900, 0x9933};
 
-        //ダイアログを表示する
         if (cd.ShowDialog() == DialogResult.OK)
         {
-            //選択された色の取得
-            LabelColorTxtBox.Text = cd.Color.ToString();
+            LabelColorTxtBox.Text = cd.Color.Name;
         }
     }
     private void RootSelectBtn_Click(Object? sender, EventArgs? e)
@@ -146,6 +190,22 @@ public partial class MainForm : Form
             FileTreeView.Path = folderBrowserDialog1.SelectedPath + "\\";
             RootPathTxtBox.Text = FileTreeView.Path;
         }
+    }
+
+    private Color String2Color(string strColor)
+    {
+        Color color;
+
+        try
+        {
+            color = ColorTranslator.FromHtml(strColor);
+        }
+        catch
+        {
+            strColor = "#" + strColor;
+            color = ColorTranslator.FromHtml(strColor);
+        }
+        return color;
     }
 
 }
