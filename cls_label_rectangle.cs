db5abc24adf4ee35e6050f8641d25f69b2d5278d
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-02-14 22:58:08 +0900

    Update cls_label_rectangle.cs

--- cls_label_rectangle.cs
+++ cls_label_rectangle.cs
@@ -15,6 +15,8 @@ namespace IMG_Gen2
         private System.Drawing.Drawing2D.Matrix? mat;
         private Point memPos;
         private bool changeFlag = false;
+        private Point memP1;
+        private Point memP2;
 
 
         public cls_label_rectangle(cls_posPicBox posPicBox, Graphics g, string labelName, string strColor, string strWidth, int x1, int y1, int x2, int y2)
@@ -31,7 +33,7 @@ namespace IMG_Gen2
 
             btn.Visible = false;
             posPicBox.Controls.Add(btn);
-            selectBox = new(posPicBox, pos, size);
+            selectBox = new(this, posPicBox, pos, size);
 
             btn.Click += new EventHandler(Btn_Click);
             btn.MouseDown += new MouseEventHandler(Btn_MouseDown);
@@ -121,6 +123,63 @@ namespace IMG_Gen2
                 color = ColorTranslator.FromHtml(strColor);
             }
             return color;
+        }
+        internal void SelectboxMouseDown(object? sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && selectFlag)
+            {
+                memPos.X = e.X;
+                memPos.Y = e.Y;
+                memP1.X = pos.X;
+                memP1.Y = pos.Y;
+                memP2.X = pos.X + size.Width;
+                memP2.Y = pos.Y + size.Height;
+
+                changeFlag = true;
+            }
+        }
+        internal void SelectboxMouseUp(object? sender, MouseEventArgs e)
+        {
+            if (changeFlag)
+            {
+                posPicBox.SaveLabel();
+                changeFlag = false;
+            }
+        }
+        internal void SelectboxMouseMove(object? sender, MouseEventArgs e)
+        {
+            Panel? move_selectbox = sender as Panel;
+
+            if (e.Button == MouseButtons.Left && changeFlag)
+            {
+                Point newPos = new(e.X - memPos.X + move_selectbox!.Location.X, e.Y - memPos.Y + move_selectbox.Location.Y);
+                move_selectbox.Location = newPos;
+
+                switch (move_selectbox!.TabIndex)
+                {
+                    case 0:
+                        pos.X = (int)((newPos.X + 8 - mat!.Elements[4]) / mat.Elements[0]);
+                        pos.Y = (int)((newPos.Y + 8 - mat!.Elements[5]) / mat.Elements[0]);
+                        size.Width = (int)(memP2.X - pos.X);
+                        size.Height = (int)(memP2.Y - pos.Y);
+                        break;
+                    case 1:
+                        pos.Y = (int)((newPos.Y + 8 - mat!.Elements[5]) / mat.Elements[0]);
+                        size.Width = (int)((int)((newPos.X - mat!.Elements[4]) / mat.Elements[0]) - pos.X);
+                        size.Height = (int)(memP2.Y - pos.Y);
+                        break;
+                    case 2:
+                        pos.X = (int)((newPos.X + 8 - mat!.Elements[4]) / mat.Elements[0]);
+                        size.Width = (int)(memP2.X - pos.X);
+                        size.Height = (int)((int)((newPos.Y - mat!.Elements[5]) / mat.Elements[0]) - pos.Y);
+                        break;
+                    case 3:
+                        size.Width = (int)((int)((newPos.X - mat!.Elements[4]) / mat.Elements[0]) - pos.X);
+                        size.Height = (int)((int)((newPos.Y - mat!.Elements[5]) / mat.Elements[0]) - pos.Y);
+                        break;
+                }
+                posPicBox.DrawImage();
+            }
         }
     }
 }
