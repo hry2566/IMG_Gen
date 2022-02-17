628a5da80dc27937e2621615c46c3d67b55a0df7
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-02-17 14:45:46 +0900

    Update cls_treenode.cs

--- cls_treenode.cs
+++ cls_treenode.cs
@@ -3,36 +3,53 @@ namespace IMG_Gen2
 {
     public class cls_treenode : TreeNode
     {
-        private string nodePath;
-        private cls_treenode[] node = new cls_treenode[0];
-        public cls_treenode(string path, string nodeName)
+        cls_treenode[] node = new cls_treenode[0];
+        public cls_treenode(string nodeName, string rootPath)
         {
-            nodePath = nodeName + "\\";
-            this.Text = nodeName.Replace(path, "");
-
-            if (Directory.Exists(nodeName))
+            this.Text = nodeName;
+            if (System.IO.Directory.Exists(rootPath + nodeName))
             {
-                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(nodePath);
-                System.IO.FileInfo[] files = di.GetFiles("*.jpg", System.IO.SearchOption.AllDirectories);
-                foreach (System.IO.FileInfo f in files)
+                rootPath = rootPath + nodeName + "\\";
+                string[] subFolders = System.IO.Directory.GetDirectories(
+                rootPath, "*", System.IO.SearchOption.TopDirectoryOnly);
+
+                for (int i = 0; i < subFolders.Length;i++)
                 {
-                    Array.Resize(ref node, node.Length + 1);
-                    node[node.Length - 1] = new cls_treenode(nodePath, f.FullName);
-                }
-                files = di.GetFiles("*.bmp", System.IO.SearchOption.AllDirectories);
-                foreach (System.IO.FileInfo f in files)
-                {
-                    Array.Resize(ref node, node.Length + 1);
-                    node[node.Length - 1] = new cls_treenode(nodePath, f.FullName);
-                }
-                files = di.GetFiles("*.png", System.IO.SearchOption.AllDirectories);
-                foreach (System.IO.FileInfo f in files)
-                {
-                    Array.Resize(ref node, node.Length + 1);
-                    node[node.Length - 1] = new cls_treenode(nodePath, f.FullName);
+                    string[] split = subFolders[i].Split("\\");
+                    string folderName = split[split.Length - 1];
+
+                    if(folderName!="pos" && folderName!="mask" && folderName!="split")
+                    {
+                        Array.Resize(ref node, node.Length + 1);
+                        node[node.Length - 1] = new(folderName, rootPath);
+                    }
                 }
                 this.Nodes.AddRange(node);
+                node= AddFileNode(rootPath, "*.jpg");
+                this.Nodes.AddRange(node);
+                node= AddFileNode(rootPath, "*.bmp");
+                this.Nodes.AddRange(node);
+                node= AddFileNode(rootPath, "*.png");
+                this.Nodes.AddRange(node);
+                node= AddFileNode(rootPath, "*.gif");
+                this.Nodes.AddRange(node);
             }
+        }
+        internal static cls_treenode[] AddFileNode(string rootPath,string fileExp)
+        {
+            cls_treenode[] node = new cls_treenode[0];
+
+            string[] subfiles = System.IO.Directory.GetFiles(
+                rootPath, fileExp, System.IO.SearchOption.TopDirectoryOnly);
+                for (int i = 0; i < subfiles.Length;i++)
+                {
+                    string[] split = subfiles[i].Split("\\");
+                    string fileName = split[split.Length - 1];
+
+                    Array.Resize(ref node, node.Length + 1);
+                    node[node.Length - 1] = new(fileName, rootPath);
+                }
+            return node;
         }
     }
 }
