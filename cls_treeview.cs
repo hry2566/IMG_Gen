53966befd67d7eb69fb87dcebd346fdce2b86099
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-02-21 15:41:12 +0900

    Update cls_treeview.cs

--- cls_treeview.cs
+++ cls_treeview.cs
@@ -6,15 +6,17 @@ public partial class cls_treeview : TreeView
     private TabPage PosPage;
     private cls_image_BrightContrast Image_BrightContrast;
     private cls_image_RandomNoise? Image_RandomNoise;
+    private cls_image_Split? Image_Split;
     private string? rootPath;                               // ルートフォルダパス
     private cls_treenode[] treeNode = new cls_treenode[0];
 
-    public cls_treeview(cls_posPicBox picBox1, TabPage PosPage, cls_image_BrightContrast Image_BrightContrast, cls_image_RandomNoise Image_RandomNoise)
+    public cls_treeview(cls_posPicBox picBox1, TabPage PosPage, cls_image_BrightContrast Image_BrightContrast, cls_image_RandomNoise Image_RandomNoise,cls_image_Split Image_Split)
     {
         this.picBox1 = picBox1;
         this.PosPage = PosPage;
         this.Image_BrightContrast = Image_BrightContrast;
         this.Image_RandomNoise = Image_RandomNoise;
+        this.Image_Split = Image_Split;
         this.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(SelectChange);
     }
     private void SelectChange(Object? sender, TreeViewEventArgs e)
@@ -27,6 +29,7 @@ public partial class cls_treeview : TreeView
             picBox1.SetImage(filePath, rootPath!);
             Image_BrightContrast.SetImage(filePath);
             Image_RandomNoise!.SetImage(filePath);
+            Image_Split!.SetImage(filePath);
         }
     }
     internal string Path
