8b77f832d6713fc9ca37061678ac86f7365bfb24
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-02-23 09:50:33 +0900

    Update cls_treeview.cs

--- cls_treeview.cs
+++ cls_treeview.cs
@@ -3,33 +3,53 @@ namespace IMG_Gen2;
 public partial class cls_treeview : TreeView
 {
     private cls_posPicBox picBox1;
-    private TabPage PosPage;
+    private TabControl ViewTab;
     private cls_image_BrightContrast Image_BrightContrast;
     private cls_image_RandomNoise? Image_RandomNoise;
     private cls_image_Split? Image_Split;
     private string? rootPath;                               // ルートフォルダパス
     private cls_treenode[] treeNode = new cls_treenode[0];
 
-    public cls_treeview(cls_posPicBox picBox1, TabPage PosPage, cls_image_BrightContrast Image_BrightContrast, cls_image_RandomNoise Image_RandomNoise,cls_image_Split Image_Split)
+    public cls_treeview(cls_posPicBox picBox1, TabControl ViewTab, cls_image_BrightContrast Image_BrightContrast, cls_image_RandomNoise Image_RandomNoise, cls_image_Split Image_Split)
     {
         this.picBox1 = picBox1;
-        this.PosPage = PosPage;
+        this.ViewTab = ViewTab;
         this.Image_BrightContrast = Image_BrightContrast;
         this.Image_RandomNoise = Image_RandomNoise;
         this.Image_Split = Image_Split;
         this.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(SelectChange);
+        this.ViewTab.SelectedIndexChanged += new EventHandler(ViewTab_SelectedIndexChanged);
+    }
+    private void ViewTab_SelectedIndexChanged(Object? sender, EventArgs e)
+    {
+        if(this.SelectedNode==null){return;}
+        string filePath = rootPath + this.SelectedNode.FullPath;
+        SwitchImage(ViewTab.SelectedIndex, filePath);
+    }
+    private void SwitchImage(int selectIndex, string filePath)
+    {
+        if (!System.IO.File.Exists(filePath)) { return; }
+        
+        switch(ViewTab.SelectedIndex)
+        {
+            case 0:
+                picBox1.SetImage(filePath, rootPath!);
+                break;
+            case 1:
+                Image_BrightContrast.SetImage(filePath);
+                Image_RandomNoise!.SetImage(filePath);
+                break;
+            case 2:
+                Image_Split!.SetImage(filePath, rootPath!);
+                break;
+        }
     }
     private void SelectChange(Object? sender, TreeViewEventArgs e)
     {
         if (e.Node!.Text.IndexOf(".") > -1)
         {
             string filePath = rootPath + e.Node!.FullPath;
-
-            if (!System.IO.File.Exists(filePath)) { return; }
-            picBox1.SetImage(filePath, rootPath!);
-            Image_BrightContrast.SetImage(filePath);
-            Image_RandomNoise!.SetImage(filePath);
-            Image_Split!.SetImage(filePath,rootPath!);
+            SwitchImage(ViewTab.SelectedIndex, filePath);
         }
     }
     internal string Path
