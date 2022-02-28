2fe682051bfec68c1218830ae66f59acf1103be4
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-02-28 14:30:39 +0900

    Update cls_image_RandomNoise.cs

--- cls_image_RandomNoise.cs
+++ cls_image_RandomNoise.cs
@@ -3,20 +3,21 @@ namespace IMG_Gen2
 {
     public class cls_image_RandomNoise
     {
-        private string? filePath;
-        private CheckBox? RndNoiseChkBox;
-        private HScrollBar? RndNoiseScrBar;
-        private HScrollBar? RndNoiseRatioScrBar;
-        private TextBox? RndNoiseTxtBox;
-        private TextBox? RndNoiseRatioTxtBox;
-        private Button? RndNoisePreviewBtn;
-        private PictureBox? PicBox2;
-        private Boolean readFlag = false;
-        private Boolean runFlag = false;
+        private string? filePath;                           // ファイルパス
+        private CheckBox? RndNoiseChkBox;                   // ランダムノイズ有効／無効
+        private HScrollBar? RndNoiseScrBar;                 // ノイズ値
+        private HScrollBar? RndNoiseRatioScrBar;            // レシオ値
+        private TextBox? RndNoiseTxtBox;                    // ノイズ表示
+        private TextBox? RndNoiseRatioTxtBox;               // レシオ表示
+        private Button? RndNoisePreviewBtn;                 //　ランダムノイズ表示
+        private PictureBox? PicBox2;                        // 画像表示PictureBox
+        private Boolean readFlag = false;                   // ファイル読込フラグ
+        private Boolean runFlag = false;                    // 実行フラグ
         private Bitmap? bmp;                                // 表示するBitmap
         private Graphics? g;                                // 描画用Graphicsオブジェクト
         private System.Drawing.Drawing2D.Matrix? mat;       // アフィン変換行列
 
+        // コンストラクタ
         public cls_image_RandomNoise(List<Control> imgCtrl)
         {
             RndNoiseChkBox = imgCtrl[0] as CheckBox;
@@ -38,6 +39,10 @@ namespace IMG_Gen2
 
             ReadImageIni("./ini/image_random_noise.ini");
         }
+
+        //*************************************************************************
+        // Events(cls_image_RandomNoise)
+        //*************************************************************************
         private void  RndNoiseChkBox_Click(object? sender, EventArgs e)
         {
             SaveImageIni("./ini/image_random_noise.ini");
@@ -67,6 +72,48 @@ namespace IMG_Gen2
             BmpReadFile(filePath);
             mat = new System.Drawing.Drawing2D.Matrix();
         }
+        private void RndNoisePreviewBtn_Click(Object? sender, EventArgs e)
+        {
+            if (PicBox2!.Image == null) { return; }
+
+            runFlag = true;
+            RndNoisePreviewBtn!.Visible = false;
+            Random rnd = new System.Random();
+            int noise = rnd.Next(128, RndNoiseScrBar!.Value);
+            int ratio = rnd.Next(RndNoiseRatioScrBar!.Value, 50);
+
+            CreateNoise(noise, ratio);
+            ImageReset();
+            RndNoisePreviewBtn!.Visible = true;
+            runFlag = false;
+        }
+        private void RndNoise_HScrBar_ValueChanged(Object? sender, EventArgs e)
+        {
+            HScrollBar? ctrl = sender as HScrollBar;
+            string ctrlName = ctrl!.Name.Replace("ScrBar", "TxtBox");
+            TextBox[] txtBox = new TextBox[2] { RndNoiseTxtBox!, RndNoiseRatioTxtBox! };
+
+            for (int i = 0; i < 2; i++)
+            {
+                if (txtBox[i].Name == ctrlName)
+                {
+                    txtBox[i].Text = ctrl.Value.ToString();
+                    if (!runFlag)
+                    {
+                        RandomNoise_View();
+                    }
+                    break;
+                }
+            }
+            if (!readFlag)
+            {
+                SaveImageIni("./ini/image_random_noise.ini");
+            }
+        }
+
+        //*************************************************************************
+        // 関数(ファイル関連)
+        //*************************************************************************
         private void ReadImageIni(string iniFileName)
         {
             if (!cls_posPicBox.CheckFile(iniFileName)) { return; }
@@ -103,49 +150,14 @@ namespace IMG_Gen2
             sw.Flush();
             sw.Close();
         }
-        private void RndNoisePreviewBtn_Click(Object? sender, EventArgs e)
-        {
-            if (PicBox2!.Image == null) { return; }
 
-            runFlag = true;
-            RndNoisePreviewBtn!.Visible = false;
-            Random rnd = new System.Random();
-            int noise = rnd.Next(128, RndNoiseScrBar!.Value);
-            int ratio = rnd.Next(RndNoiseRatioScrBar!.Value, 50);
-
-            CreateNoise(noise, ratio);
-            ImageReset();
-            RndNoisePreviewBtn!.Visible = true;
-            runFlag = false;
-        }
-
+        //*************************************************************************
+        // 関数（Image関連）
+        //*************************************************************************
         private void CreateNoise(int noise, int ratio)
         {
             BmpReadFile(filePath!);
             AddNoise(bmp!, noise, ratio);
-        }
-        private void RndNoise_HScrBar_ValueChanged(Object? sender, EventArgs e)
-        {
-            HScrollBar? ctrl = sender as HScrollBar;
-            string ctrlName = ctrl!.Name.Replace("ScrBar", "TxtBox");
-            TextBox[] txtBox = new TextBox[2] { RndNoiseTxtBox!, RndNoiseRatioTxtBox! };
-
-            for (int i = 0; i < 2; i++)
-            {
-                if (txtBox[i].Name == ctrlName)
-                {
-                    txtBox[i].Text = ctrl.Value.ToString();
-                    if (!runFlag)
-                    {
-                        RandomNoise_View();
-                    }
-                    break;
-                }
-            }
-            if (!readFlag)
-            {
-                SaveImageIni("./ini/image_random_noise.ini");
-            }
         }
         private void RandomNoise_View()
         {
@@ -159,9 +171,45 @@ namespace IMG_Gen2
             ImageReset();
             runFlag = false;
         }
-        private void AddNoise(Bitmap bmp, int noise, int ratio)
+        // private void AddNoise(Bitmap bmp, int noise, int ratio)
+        // {
+        //     if (bmp == null) { return; }
+        //     int w = bmp.Width, h = bmp.Height;
+        //     Random rnd = new System.Random();
+        //     int iRnd = 0;
+        //     int rgbPos = 0;
+        //     int flag = 0;
+
+        //     for (int x = 0; x < w; x++)
+        //     {
+        //         for (int y = 0; y < h; y++)
+        //         {
+        //             Color pixel = bmp.GetPixel(x, y);
+        //             iRnd = rnd.Next(-noise, noise);
+        //             flag = rnd.Next(0, ratio);
+        //             rgbPos = rnd.Next(0, 2);
+
+        //             // ARGB
+        //             byte[] RGB = new byte[3];
+        //             RGB[2] = pixel.B;
+        //             RGB[1] = pixel.G;
+        //             RGB[0] = pixel.R;
+
+        //             iRnd = rnd.Next(-noise, noise);
+        //             flag = rnd.Next(0, ratio);
+        //             rgbPos = rnd.Next(0, 2);
+        //             if (flag == 0)
+        //             {
+        //                 RGB[rgbPos] = (byte)(RGB[rgbPos] + iRnd);
+        //                 pixel = Color.FromArgb(RGB[0], RGB[1], RGB[2]);
+        //                 bmp.SetPixel(x, y, pixel);
+        //             }
+        //         }
+        //     }
+        // }
+        internal Bitmap AddNoise(Bitmap bmp, int noise, int ratio)
         {
-            if (bmp == null) { return; }
+            if (bmp == null) { return null!; }
             int w = bmp.Width, h = bmp.Height;
             Random rnd = new System.Random();
             int iRnd = 0;
@@ -194,6 +242,7 @@ namespace IMG_Gen2
                     }
                 }
             }
+            return bmp;
         }
         private void ImageReset()
         {
@@ -236,12 +285,7 @@ namespace IMG_Gen2
             Image image = Image.FromFile(filePath);
             bmp = new Bitmap(image);
             image.Dispose();
-            // PictureBox pic1 = new();
-            // pic1.Image = new Bitmap(filePath);
-            // bmp = new Bitmap(pic1.Image);
-            // pic1.Image.Dispose();
         }
-
         internal int GetNoise()
         {
             return RndNoiseScrBar!.Value;
@@ -254,6 +298,5 @@ namespace IMG_Gen2
         {
             return RndNoiseChkBox!.Checked;
         }
-
     }
 }
